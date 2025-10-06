ea5f820db23dd42ee127d413c9eb2d39b92555be
msarica <msarica1@umbc.edu>
2025-10-06 17:33:05 -0400

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/morse_code/',
+  base: '/mesuds_morse_code/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
